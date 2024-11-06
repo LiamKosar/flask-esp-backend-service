0f374ab11c10ada031c1371ba1d0dcad99ce8541
Liam Kosar <kosar.liam@gmail.com>
2024-11-06 10:15:38 -0500

    Fasfsfsd

--- api/database_setup.py
+++ api/database_setup.py
@@ -1,15 +1,8 @@
 import os
-from pony.orm import Database, Required, db_session
-
+from pony.orm import Database
 
 def connect_to_database():
     db = Database()
-    # db.bind(
-    #     provider='postgres',
-    #     user='defualt',
-    #     password='5zsRvYe7MBtH',
-    #     host='ep-calm-grass-a4wyu5pn-pooler.us-east-1.aws.neon.tech',
-    #     database='verceldb',
-    # )
-    db.bind(provider='postgres', dsn='postgres://default:5zsRvYe7MBtH@ep-calm-grass-a4wyu5pn-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
+    POSTGRES_URL = os.getenv('POSTGRES_URL')
+    db.bind(provider='postgres', dsn=POSTGRES_URL)
     return db
